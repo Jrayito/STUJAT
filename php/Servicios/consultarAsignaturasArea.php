cfb464f7d038c4e1e600e619bc1fd40d7fc052b8
Jrayito <jesuraymundi@gmail.com>
2022-06-24 13:25:05 -0500

    Se agrego la busqueda por ciclo corto

--- php/Servicios/consultarAsignaturasArea.php
+++ php/Servicios/consultarAsignaturasArea.php
@@ -11,8 +11,13 @@
 
     $index = $_POST['index'];
     $cargadas = $_POST['filtro'];
+    $op = $_POST['option'];
 
-    $sql = "SELECT* FROM asignaturas WHERE areaConocimiento = $index AND (clave NOT IN ($cargadas) AND optativa = 0)";
+    $sql = (intval($op)) 
+            ? "SELECT * FROM asignaturas WHERE ciclo = 1"
+            : "SELECT * FROM asignaturas 
+                WHERE areaConocimiento = $index 
+                AND (clave NOT IN ($cargadas) AND optativa = 0)";
 
     $response->data = $asignatura->consultarAsignaturas($conexion, $sql);
     echo json_encode($response);
